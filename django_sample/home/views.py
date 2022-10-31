37ab5eb7507d0231cb9284461a968eef8f034178
Nidhin P Pramod <kichum386@gamil.com>
2022-10-31 21:37:02 +0530

    Studied to create models and completed CRUD operations

--- django_sample/home/views.py
+++ django_sample/home/views.py
@@ -1,14 +1,11 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 
+from .models import Departments
+
 # Create your views here.
 def index(request):
-    numbers={
-        "num":[1,2,3,4,5,6],
-        "fruits":['banana','apple'],
-    }
-
-    return render(request, "index.html",numbers)
+    return render(request, "index.html")
 
 def about(request):
      return render(request, "about.html")
@@ -23,4 +20,8 @@ def contact(request):
     return render(request, "contact.html")
 
 def department(request):
-    return render(request, "department.html")
+    dict_dept={
+        'dept':Departments.objects.all()
+    }
+
+    return render(request, "department.html",dict_dept)
