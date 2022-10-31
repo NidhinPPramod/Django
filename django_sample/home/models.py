b7bd70f32d4f2a02947591b8882fd29cf658a32b
Nidhin P Pramod <kichum386@gamil.com>
2022-10-31 22:38:53 +0530

    Studied to add images

--- django_sample/home/models.py
+++ django_sample/home/models.py
@@ -6,8 +6,14 @@ class Departments(models.Model):
     dep_name=models.CharField(max_length=100)
     dep_description=models.TextField()
 
+    def __str__(self):
+        return self.dep_name
+
 class Doctors(models.Model):
     doc_name=models.CharField(max_length=100)
     doc_spec=models.TextField(max_length=255)
     dep_name=models.ForeignKey(Departments, on_delete=models.CASCADE)
     doc_image=models.ImageField(upload_to='doctors')
+
+    def __str__(self):
+        return self.doc_name
--- django_sample/home/views.py
+++ django_sample/home/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 
-from .models import Departments
+from .models import Departments,Doctors
 
 # Create your views here.
 def index(request):
@@ -14,7 +14,11 @@ def booking(request):
      return render(request, "booking.html")
 
 def doctors(request):
-     return render(request, "doctor.html")
+     dict_doctors={
+        "doc":Doctors.objects.all()
+     }
+
+     return render(request, "doctor.html",dict_doctors)
 
 def contact(request):
     return render(request, "contact.html")
