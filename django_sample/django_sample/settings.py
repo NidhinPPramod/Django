90a9697a73f21e1b78cf384d011b23d7581f2e7a
Nidhin P Pramod <kichum386@gamil.com>
2022-11-01 21:06:51 +0530

    Studied About djnago forms and how to post forms

--- django_sample/django_sample/settings.py
+++ django_sample/django_sample/settings.py
@@ -38,7 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'home',
+    'home','crispy_forms'
 ]
 
 MIDDLEWARE = [
@@ -130,3 +130,5 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 MEDIA_ROOT=BASE_DIR/'uploads'
 MEDIA_URL= '/media/'
+
+CRISPY_TEMPLATE_PACK = 'bootstrap4'
--- django_sample/home/models.py
+++ django_sample/home/models.py
@@ -16,4 +16,15 @@ class Doctors(models.Model):
     doc_image=models.ImageField(upload_to='doctors')
 
     def __str__(self):
-        return self.doc_name
+        return self.doc_name 
+
+class Booking(models.Model):
+    p_name=models.CharField(max_length=100)
+    p_phone=models.CharField(max_length=10)
+    p_email=models.EmailField()
+    doc_name=models.ForeignKey(Doctors, on_delete=models.CASCADE)
+    booking_date=models.DateField()
+    booked_on=models.DateField(auto_now=True)
+
+    def __str__(self):
+        return self.p_name
